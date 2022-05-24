7a098ab845d87d80512953501a9b4f5fcf481eb4
meta-ela <anxhelameta@live.it>
2022-05-24 15:52:47 +0200

    Create repo and exercise 1

---
+++ snack1/app.js
@@ -0,0 +1,17 @@
+/*
+Snack1
+Inserisci un numero.
+se è pari stampa il numero
+se è dispari stampa il numero successivo.
+
+// soluzione
+- creare basic layout con input
+- prelevare dati input inseriti dall'user
+- dichiarare i dati in una variabile
+- creare un ciclo for da 0 a 1000 numeri
+- richiamare dentro al ciclo la variabile dandogli il value/parseInt ?????
+- creare IF 
+    - divisibile (% 2 === 0) allora stampare il numero dato nell'input-result
+    ALTRIMENTI
+        - SE non DIVISIBILE stampare il numero successivo.
+*/
