befee639eff1d1b1da6a224126eee4cd27f403d1
meta-ela <anxhelameta@live.it>
2022-05-24 20:05:37 +0200

    Adds exercise 2 and 3

--- snack2/app.js
+++ snack2/app.js
@@ -4,8 +4,52 @@ Generatore di “nomi cognomi” casuali:
 il Grande Gatsby ha  una lista di nomi e una lista di cognomi, 
 e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
+
+// soluzione
+- due array con nomi e cognomi causali
+- 
 */
 
-const listaNomi = [];
+const listaNomi = ["Maria", 
+                    "Giulia", 
+                    "Sara", 
+                    "Lucia", 
+                    "Marta", 
+                    "Elena", 
+                    "Serena", 
+                    "Rossella", 
+                    "Micol",
+                    "Francesca",
+                    "Luca", 
+];
 
-const listaCognomi = [];
+const listaCognomi = ["Rossi", 
+                        "Verdi", 
+                        "Gialli", 
+                        "Cosimi", 
+                        "Dardi", 
+                        "Fornito", 
+                        "Civolani", 
+                        "Copa", 
+                        "Cappiello", 
+                        "Stoccafisso", 
+                        "Lardo", 
+];
+
+
+// minimo 0 e massimo 10
+// max = 11;
+
+// formula random
+// Math.floor(Math.random() * max);
+
+let fullName = "";
+let randomOne = Math.floor(Math.random() * listaNomi.length);
+let randomTwo = Math.floor(Math.random() * listaNomi.length);
+
+console.log(randomOne);
+console.log(randomTwo);
+
+fullName = listaNomi[randomOne] + " " + listaCognomi[randomTwo];
+
+console.log(fullName);
--- snack3/app.js
+++ snack3/app.js
@@ -1,6 +1,31 @@
 /*
 Snack3
 Crea un array di numeri interi
-e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
+e fai la somma di tutti gli elementi 
+che sono in posizione (indice) dispari
 
+
+// soluzione
+- creare array
+- 
 */
+
+let numberArray = [];
+
+// creazione array numeri
+for (i = 0; i < 1000; i++) {
+
+    numberArray.push(Math.floor(Math.random() * i));
+}
+
+let sommaNumeriDispari = 0;
+
+// somma dei numeri in pos. indice dispari
+for (i = 0; i < numberArray.length; i++) {
+    // controllo se indice dispari
+    if (!(i % 2 === 0)) {
+        sommaNumeriDispari = sommaNumeriDispari + numberArray[i];
+    }
+}
+
+console.log(sommaNumeriDispari);
